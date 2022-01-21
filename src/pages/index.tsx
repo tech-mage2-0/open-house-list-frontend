1573b3e95ddca892b476dbd779a7c0ed5dd9c488
Ramon <ramon1234rxm@gmail.com>
2022-01-21 14:35:11 -0300

    feat: finish implementation and poc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,33 @@
+import { useFetchList } from 'services/hooks/useFetchList';
+import { useListStore } from 'store/useList';
+
 export default function Main() {
-  return <div>Hello, world!</div>;
+  const { list, addListItems, clearList } = useListStore();
+  const { data, error, isLoading } = useFetchList();
+
+  if (isLoading) return <div>Loading..</div>;
+  if (error) return <div>Erro..</div>;
+
+  return (
+    <div>
+      {list.map((item) => (
+        <div key={Math.random()}>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+          <address>
+            {`${item.address} - ${item.latitude.toFixed(
+              2
+            )} - ${item.longitude.toFixed(2)}
+            `}
+          </address>
+        </div>
+      ))}
+      <button type="button" onClick={() => addListItems(data)}>
+        Adicionar item a lista
+      </button>
+      <button type="button" onClick={clearList}>
+        Limpar lista
+      </button>
+    </div>
+  );
 }
